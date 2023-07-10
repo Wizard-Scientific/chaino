47a0ed7362021b7d99ae1f16207a8eb11d2fe17c
0xidm <0xidm@protonmail.com>
2023-07-10 19:15:00 -0400

    tidy

--- src/chaino/grouped_multicall.py
+++ src/chaino/grouped_multicall.py
@@ -1,9 +1,10 @@
 import random
 import string
 
-from multicall import Call, Multicall, Signature
-from multicall.signature import parse_signature
+from multicall import Call, Multicall
 from web3 import Web3
+
+from .utils import convert_signature_to_abi
 
 
 class GroupedMulticall:
@@ -11,30 +12,20 @@ class GroupedMulticall:
         self.w3 = w3
         self.margin = 0.1
 
-    def estimate_gas(self, function, contract_address, input_value):
+    def get_max_items(self, function, contract_address, input_value):
+        # estimate gas used for a single call
         function_abi = convert_signature_to_abi(function)
         contract = self.w3.eth.contract(contract_address, abi=[function_abi])
         fn = contract.functions[function_abi["name"]]
-        return fn(input_value).estimate_gas()
+        gas_estimate = fn(input_value).estimate_gas()
 
-    def get_gas_limit(self, function, contract_address, input_value):
-        return Multicall([Call(
+        # obtain gas limit from multicall
+        gas_limit = Multicall([Call(
             contract_address,
             [function, input_value],
             [('result', None)]
         )], _w3=self.w3).gas_limit
 
-    def get_max_items(self, function, contract_address, input_value):
-        gas_estimate = self.estimate_gas(
-            function,
-            contract_address,
-            input_value,
-        )
-        gas_limit = self.get_gas_limit(
-            function,
-            contract_address,
-            input_value,
-        )
         return int(gas_limit / gas_estimate * (1 - self.margin))
 
     def __call__(self, function_vector, contract_address_vector, input_vector):
@@ -65,36 +56,14 @@ class GroupedMulticall:
                 contract_calls = []
 
             if input_value:
-                one_call = Call(
-                    contract_address,
-                    [function, input_value],
-                    [(input_value, None)]
+                contract_calls.append(
+                    Call(
+                        contract_address,
+                        [function, input_value],
+                        [(input_value, None)]
+                    )
                 )
-                contract_calls.append(one_call)
 
         # yield the last one
         if len(contract_calls) > 0:
             yield Multicall(contract_calls, _w3=self.w3)
-
-
-def convert_signature_to_abi(signature):
-    def get_next_input():
-        counter = 0
-        while True:
-            counter += 1
-            yield f"_i{counter:03d}"
-
-    def get_next_output():
-        counter = 0
-        while True:
-            counter += 1
-            yield f"_o{counter:03d}"
-
-    function, input_types, output_types = parse_signature(signature)
-    function_abi = {
-        "name": function.split("(")[0],
-        "type": "function",
-        "inputs": [{"type": input_type, "name": next_name} for input_type, next_name in zip(input_types, get_next_input())],
-        "outputs": [{"type": output_type, "name": next_name} for output_type, next_name in zip(output_types, get_next_output())],
-    }
-    return function_abi
--- src/chaino/utils.py
+++ src/chaino/utils.py
@@ -1,6 +1,7 @@
 import json
 
 import pandas as pd
+from multicall.signature import parse_signature
 
 
 def convert_json_to_csv(in_file):
@@ -8,3 +9,26 @@ def convert_json_to_csv(in_file):
         data = json.load(f)
     df = pd.DataFrame(data)
     df.to_csv(in_file.replace('.json', '.csv'), index=False)
+
+
+def convert_signature_to_abi(signature):
+    def get_next_input():
+        counter = 0
+        while True:
+            counter += 1
+            yield f"_i{counter:03d}"
+
+    def get_next_output():
+        counter = 0
+        while True:
+            counter += 1
+            yield f"_o{counter:03d}"
+
+    function, input_types, output_types = parse_signature(signature)
+    function_abi = {
+        "name": function.split("(")[0],
+        "type": "function",
+        "inputs": [{"type": input_type, "name": next_name} for input_type, next_name in zip(input_types, get_next_input())],
+        "outputs": [{"type": output_type, "name": next_name} for output_type, next_name in zip(output_types, get_next_output())],
+    }
+    return function_abi
