47fbad0f5ed8f1d9740fbee5377e202ba71c2810
0xidm <0xidm@protonmail.com>
2023-07-24 13:44:17 -0400

    option to skip check existing

--- bin/blockchain.py
+++ bin/blockchain.py
@@ -21,17 +21,22 @@ def cli():
 @click.argument('block_start', type=int)
 @click.argument('block_end', type=int)
 @click.argument('filestore', type=str)
-def download(chain, block_start, block_end, filestore):
+@click.option('no_check_existing', '--no-check-existing', is_flag=True, default=True)
+def download(chain, block_start, block_end, filestore, no_check_existing):
     "Write blocks from blockchain to disk"
+
     block_scheduler = BlockScheduler(
         chain=chain,
         filestore_path=filestore,
         hierarchy_order=[3, 3, 3],
     )
 
-    logging.getLogger("chaino").info(f"Scan unfetched blocks from {block_start} to {block_end}")
+    logging.getLogger("chaino").info(f"Scan blocks from {block_start} to {block_end}")
     for block_number in range(block_start, block_end):
-        block_scheduler.add_task(block_number=block_number)
+        block_scheduler.add_task(
+            block_number=block_number,
+            check_existing=no_check_existing
+        )
 
     block_scheduler.start()
 
